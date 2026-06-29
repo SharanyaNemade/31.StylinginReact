9bfc2075274a39a71b3911ee966b6872a9d8e47a
SharanyaNemade <sharanyanemade123@gmail.com>
2026-06-29 15:12:29 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,114 @@
 import { useState } from "react";
 import "./App.css";
-import Radio_DropDown from "./Radio_DropDown";
-
-
 
 function App() {
 
+  // const userData = ['John', 'Jane', 'Alice', 'Bob'];
+
+  const userData = [
+    { 
+      id: 1,
+      name: 'John', 
+      age: 25, 
+      email: 'john@example.com'   
+    },
+
+    {
+      id: 2,
+      name: 'Jane', 
+      age: 30, 
+      email: 'jane@example.com'
+    },
+
+    {
+      id: 3,
+      name: 'Alice', 
+      age: 28, 
+      email: 'alice@example.com'
+    },
+
+    {
+      id: 4,
+      name: 'Bob', 
+      age: 35, 
+      email: 'bob@example.com'
+    }
+  ];
+
+
+
   return (
     <div>
-      <Radio_DropDown />
+      <h1>Loop in JSX with Map Function</h1>
+      <table border="1">
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Email</td>
+          </tr>
+        </thead>
+
+        <tbody>
+          {
+            userData.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.age}</td>
+                <td>{item.email}</td>
+              </tr>
+            ))
+          }  
+        </tbody>  
+      </table>
+
+    
+
+      <h1>Dummy Data Without Loop</h1>
+      <table border="1">
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Email</td>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Anil</td>
+            <td>25</td>
+            <td>anil@test.com</td>
+          </tr>
+
+          <tr>
+            <td>2</td>
+            <td>Anil</td>
+            <td>25</td>
+            <td>anil@test.com</td>
+          </tr>
+
+          <tr>
+            <td>3</td>
+            <td>Anil</td>
+            <td>25</td>
+            <td>anil@test.com</td>
+          </tr>
+
+          <tr>
+            <td>4</td>
+            <td>Anil</td>
+            <td>25</td>
+            <td>anil@test.com</td>
+          </tr>
+
+        </tbody>
+
+      </table>
     </div>
   );
 }
