685f9fe69c20f1ea70c92cbfa5e8be791cc4309a
SharanyaNemade <sharanyanemade123@gmail.com>
2026-06-29 20:51:03 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,114 +1,26 @@
 import { useState } from "react";
 import "./App.css";
+import ReuseComponent from "./ReuseComponent";
 
 function App() {
 
-  // const userData = ['John', 'Jane', 'Alice', 'Bob'];
-
   const userData = [
-    { 
-      id: 1,
-      name: 'John', 
-      age: 25, 
-      email: 'john@example.com'   
-    },
-
-    {
-      id: 2,
-      name: 'Jane', 
-      age: 30, 
-      email: 'jane@example.com'
-    },
-
-    {
-      id: 3,
-      name: 'Alice', 
-      age: 28, 
-      email: 'alice@example.com'
-    },
-
-    {
-      id: 4,
-      name: 'Bob', 
-      age: 35, 
-      email: 'bob@example.com'
-    }
-  ];
-
+  { id: 1, name: "John", age: 25, email: "john@example.com" },
+  { id: 2, name: "Jane", age: 30, email: "jane@example.com" },
+  { id: 3, name: "Alice", age: 28, email: "alice@example.com" },
+  { id: 4, name: "Bob", age: 35, email: "bob@example.com" }
+];
 
 
   return (
     <div>
-      <h1>Loop in JSX with Map Function</h1>
-      <table border="1">
-        <thead>
-          <tr>
-            <td>Id</td>
-            <td>Name</td>
-            <td>Age</td>
-            <td>Email</td>
-          </tr>
-        </thead>
-
-        <tbody>
-          {
-            userData.map((item) => (
-              <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.name}</td>
-                <td>{item.age}</td>
-                <td>{item.email}</td>
-              </tr>
-            ))
-          }  
-        </tbody>  
-      </table>
-
-    
-
-      <h1>Dummy Data Without Loop</h1>
-      <table border="1">
-        <thead>
-          <tr>
-            <td>Id</td>
-            <td>Name</td>
-            <td>Age</td>
-            <td>Email</td>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Anil</td>
-            <td>25</td>
-            <td>anil@test.com</td>
-          </tr>
-
-          <tr>
-            <td>2</td>
-            <td>Anil</td>
-            <td>25</td>
-            <td>anil@test.com</td>
-          </tr>
-
-          <tr>
-            <td>3</td>
-            <td>Anil</td>
-            <td>25</td>
-            <td>anil@test.com</td>
-          </tr>
-
-          <tr>
-            <td>4</td>
-            <td>Anil</td>
-            <td>25</td>
-            <td>anil@test.com</td>
-          </tr>
-
-        </tbody>
-
-      </table>
+      <h1>Reuse Component In Loop</h1>
+      {
+         userData.map((user) => (
+          <ReuseComponent key={user.id}
+              user={user}/>
+        ))
+      }
     </div>
   );
 }
